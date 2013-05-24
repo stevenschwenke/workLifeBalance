ec407cb2115ea53bd6da0a094f149b0d3960dac2
stevenschwenke <steven@stevenschwenke.de>
2013-05-24 08:40:54 +0200

    Refactored Start class

--- src/main/java/de/stevenschwenke/java/javafx/workLifeBalance/client/Start.java
+++ src/main/java/de/stevenschwenke/java/javafx/workLifeBalance/client/Start.java
@@ -1,4 +1,4 @@
-package de.stevenschwenke.java.javafx.workLifeBalance.server;
+package de.stevenschwenke.java.javafx.workLifeBalance.client;
 
 import javafx.application.Application;
 import javafx.scene.Group;
